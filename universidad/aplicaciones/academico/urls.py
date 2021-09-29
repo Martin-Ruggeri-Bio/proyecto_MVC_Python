f4756d206ba226532e0caaa492cd6c609ceb6f12
Martin-Ruggeri-Bio <martinruggeri18@gmail.com>
2021-09-29 09:14:38 -0300

    proyecto mvc en python

---
+++ universidad/aplicaciones/academico/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+# cuando accedamos al directorio raiz nos va a devolver una vista
+urlpatterns = [
+    path('', views.home),
+    path('registrarCurso/', views.registrarCurso),
+    path('edicionCurso/<codigo>', views.edicionCurso),
+    path('editarCurso/', views.editarCurso),
+    path('eliminarCurso/<codigo>', views.eliminarCurso)
+]
